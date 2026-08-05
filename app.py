fd1dd111da6e2cef2e5e3d087ff0f7b865b3640b
Nix-Priyanshu <nix.priyanshu@gmail.com>
2026-08-05 13:13:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from datetime import datetime
 import streamlit as st
 
 # LangChain imports (kept simple, as per project scope)
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
